bdd13ddc5e6669590379d7d63afaa991b3073e46
teodorakadic <teodorakadic@gmail.com>
2023-11-29 19:13:46 +0100

    Added Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import CircleSVG from './CircleSVG'
+import logo from './logo.svg'
+import circle from './circle.svg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app">
+			<header className="header">
+				<img src={logo} className="logo" />
+				{/* <span>fatcat</span>
+					<span className="coders">coders</span> */}
+				<div className="links">
+					<span className="link">Services</span>
+					<span className="link">Careers</span>
+					<span className="link">About Us</span>
+					<span className="link blog">Blog</span>
+					<a>
+						<span>Contact Us</span>
+						<CircleSVG />
+					</a>
+				</div>
+			</header>
+		</div>
+	)
 }
 
-export default App;
+export default App
