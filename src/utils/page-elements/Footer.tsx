10036ad9fb52ccdaf2f166c86317551c9318df63
teodorakadic <teodorakadic@gmail.com>
2023-12-01 18:07:22 +0100

    ⌛ Working on Footer

--- src/utils/page-elements/Footer.tsx
+++ src/utils/page-elements/Footer.tsx
@@ -1,13 +1,30 @@
-import '../styles/Footer.css'
+import '../../styles/Footer.css'
+import logo from '../../SVGs/logo.svg'
 
 const Footer = () => {
 	return (
 		<footer className="footer">
-			<section></section>
+			<section>
+				<div>
+					<a href="/" className="a-logo">
+						<img src={logo} className="logo" />
+					</a>
+					<p>
+						Discover our comprehensive range of services including staff
+						augmentation, software development, and hiring expertise. Get in
+						touch so we can learn more about your business needs.
+					</p>
+				</div>
+				<div></div>
+				<div></div>
+				<div></div>
+			</section>
 			<section className="footer-bottom">
 				<a>Privacy policy</a>
-				<a>© 2023 FatCat Coders. All rights reserved.</a>
-				<a href="mailto:hello@fatcatcoders.com">hello@fatcatcoders.com</a>
+				<a className="thin-text">© 2023 FatCat Coders. All rights reserved.</a>
+				<a className="thin-text" href="mailto:hello@fatcatcoders.com">
+					hello@fatcatcoders.com
+				</a>
 			</section>
 		</footer>
 	)
